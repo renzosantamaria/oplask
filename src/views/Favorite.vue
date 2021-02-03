1603fe839ec6ad97e081f5693fe91eca082ec6c5
PelleLindroth <pellelindroth@gmail.com>
2021-02-03 18:37:24 +0100

    Implement Favorite view

--- src/views/Favorite.vue
+++ src/views/Favorite.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <p v-if="!this.favorites">No Favorites</p>
+    <gallery
+      v-else
+      :images="currentFavorites"
+      @fetchNewPage="setCurrentFavorites"
+    />
   </div>
 </template>
+
+<script>
+import * as API from '../api'
+import Gallery from '../components/Gallery'
+export default {
+  components: {
+    Gallery,
+  },
+  created() {
+    this.$root.page = 1
+    this.favorites = API.getFavorites()
+
+    if (this.favorites) {
+      this.$root.totalPages = Math.ceil(this.favorites.length / 9)
+      this.currentFavorites = this.favorites.slice(0, 9)
+    }
+  },
+  computed: {
+    indexStart() {
+      return this.$root.page * 9 - 9
+    },
+  },
+  data() {
+    return {
+      favorites: [],
+      currentFavorites: [],
+    }
+  },
+  methods: {
+    setCurrentFavorites() {
+      this.currentFavorites = this.favorites.slice(
+        this.indexStart,
+        this.indexStart + 9
+      )
+    },
+  },
+}
+</script>
